8fd1eb8c6dbc2a90a2de28adad3445dc5cb6e774
Litch9x <tranducchinh20021997@gmail.com>
2025-08-07 05:06:11 +0700

    fix

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 // src/app/[locale]/page.tsx
 import { redirect } from "next/navigation";
 
-export default function Home({ params }: { params: { locale: string } }) {
-  redirect(`/${params.locale}/about`);
+export default async function Home({ params }: { params: { locale: string } }) {
+  const lang = await params.locale;
+  redirect(`/${lang}/about`);
 }
